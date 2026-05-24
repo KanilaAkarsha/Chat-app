29d141bb755eb05b732c8127ba289c889536800c
KanilaAkarsha <kanilasaman1@gmail.com>
2026-05-24 23:34:27 +0530

    cores

--- server/server.js
+++ server/server.js
@@ -59,24 +59,17 @@ io.on("connection", (socket) => {
 });
 
 app.use(express.json({ limit: "4mb" }));
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (isAllowedOrigin(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin || "*");
-    res.setHeader("Vary", "Origin");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Content-Type,Authorization,token",
-    );
-  }
-  if (req.method === "OPTIONS") return res.sendStatus(204);
-  next();
-});
+app.use(
+  cors({
+    origin: [
+      "https://chat-app-green-eight-26.vercel.app",
+
+      "http://localhost:5173",
+    ],
+
+    credentials: true,
+  }),
+);
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
 
